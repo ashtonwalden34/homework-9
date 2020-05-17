4c74be3d6dce205e2c9ac9293ba97a771f559cf0
Ashton Walden <ashtonwalden@icloud.com>
2020-05-16 23:53:31 -0600

    Added all questions & started on 'generateMarkdown' file

--- index.js
+++ index.js
@@ -1,94 +1,75 @@
 const inquirer = require("inquirer");
 const util = require("util");
 const fs = require("fs");
+const generateMardown = require("./utils/generateMarkdown");
 
 const writeFileAsync = util.promisify(fs.writeFile);
 
-/*const questions = [
+const questions = [
     {
         type: "input",
-        name: "githubUsername",
-        message: "What is your GitHub username?"
+        name: "gitHubUser",
+        message: "Please enter your github username."
     },
     {
         type: "input",
-        name: "projectTitle",
-        message: "What is the name of your project?"
-    }
+        name: "appLink",
+        message: "Please submit a link to your github repository."
+    },
+    {
+        type: "input",
+        name: "title",
+        message: "What is the title of your application?"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Please write a description of your application."
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "Please enter any instructions for installing dependencies related to your application." 
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "Please enter any instructions related to the usage of your application."
+    },
+    {
+        type: "input",
+        name: "license",
+        message: "Please enter any licenses for your application."
+    },
+    {
+        type: "input",
+        name: "contributing",
+        message: "Please enter any instructions for contributing to your application."
+    },
+    {
+        type: "input",
+        name: "tests",
+        message: "Please enter instructions for running tests on your application."
+    },
 ];
 
-function promptUser() {
-    return inquirer.prompt([
-        questions
-    ]);
-};
-*/
 
-function promptUser() {
-    return inquirer.prompt([
-        {
-            type: "input",
-            name: "githubUsername",
-            message: "What is your GitHub username?"
-        },
-        {
-            type: "input",
-            name: "project",
-            message: "What is the name of your project?"
-        }
-    ])
-    .then(function({ username }) {
-        const queryUrl = `https://api.github.com/users/${githubUsername}/repos?per_page=100`;
-
-        axios.get(queryUrl).then(function(res) {
-            const 
-        });
-    });
-};
-
-
-   
-// Using axios to call GitHub API
-axios
-    .get(https://api.github.com")
-
-
-function createReadMe(answers) {
-    return `
-    Project Title: ${answers.project}
-
-    Description:
-
-    Contributing:
-
-    Email:
-
-    Profile Pic:
-
-    Tests:
-    `
-}
-
-// May be sub for writeFileAsync
-    // fileName: README.md
-    // data: md?
-function writeToFile(fileName, data) {
-}
-
-writeToFile ();
-
-async function init() {
-    try {
-        const answers = await promptUser();
-
-        const readMe = createReadMe(answers);
-
-        await writeFileAsync("README.md", md);
-        
-        console.log("Succesfully wrote README.md");
-    } catch(err) {
+fs.writeFile("README.md", answers, function(err)
+{
+    if (err) {
         console.log(err);
+    }else {
+        console.log("success");
     }
+})
+
+
+/* function writeToFile(fileName, data) {
+    
+}*/
+
+function init() {
+
 }
 
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,11 +1,8 @@
-//const markdown-it = require("markdown-it");
-
 function generateMarkdown(data) {
-    return `
-  # ${data.title}
-  
-  `;
-  }
-  
-  module.exports = generateMarkdown;
-  
+  return `
+# ${data.title}
+
+`;
+}
+
+module.exports = generateMarkdown;
